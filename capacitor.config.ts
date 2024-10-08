67b5df210a3647e665abef7951ba2deb302b4f1d
Eduardo.Sanchez <eduardo.sanchez@madiva.com>
2024-10-08 10:12:58 +0200

    pruebas

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'shopping-list',
+  appName: 'Fe y Consuelo',
   webDir: 'www',
   server: {
     androidScheme: 'https'
